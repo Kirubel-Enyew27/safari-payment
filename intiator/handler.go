a121e7137baef13d256ab8838097eda10e76b4c9
Kirubel Enyew <enyewkirubel76@gmail.com>
2025-08-06 16:29:41 +0300

    implement handler intialization

--- intiator/handler.go
+++ intiator/handler.go
@@ -1,1 +1,20 @@
 package intiator
+
+import (
+	"time"
+
+	"github.com/Kirubel-Enyew27/safari-payment/internal/handler"
+	"github.com/Kirubel-Enyew27/safari-payment/internal/handler/payment"
+	"go.uber.org/zap"
+)
+
+type Handler struct {
+	payment handler.Payment
+}
+
+func InitHandler(service Service, log *zap.Logger, timeout time.Duration) Handler {
+
+	return Handler{
+		payment: payment.InitHandler(service.payment, timeout, log),
+	}
+}
