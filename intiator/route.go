5f39d3346216f3ddfe58e5e3fc48739452a2080d
Kirubel Enyew <enyewkirubel76@gmail.com>
2025-08-06 16:30:47 +0300

    implement intialize route

--- intiator/route.go
+++ intiator/route.go
@@ -1,1 +1,16 @@
 package intiator
+
+import (
+	"github.com/Kirubel-Enyew27/safari-payment/internal/route/payment"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func InitRouter(
+	group *gin.RouterGroup,
+	handler Handler,
+	service Service,
+	log *zap.Logger,
+) {
+	payment.InitRoute(group, handler.payment, log)
+}
