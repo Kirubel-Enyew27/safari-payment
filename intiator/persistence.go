5063a5f5ef90336aebaac7dbf345e89659500afe
Kirubel Enyew <enyewkirubel76@gmail.com>
2025-08-06 16:26:25 +0300

    implement storage layer intialization

--- intiator/persistence.go
+++ intiator/persistence.go
@@ -1,1 +1,18 @@
 package intiator
+
+import (
+	"github.com/Kirubel-Enyew27/safari-payment/internal/model/persistencedb"
+	"github.com/Kirubel-Enyew27/safari-payment/internal/storage"
+	"github.com/Kirubel-Enyew27/safari-payment/internal/storage/payment"
+	"go.uber.org/zap"
+)
+
+type Persistence struct {
+	payment storage.Payment
+}
+
+func InitPersistence(db persistencedb.PersistenceDB, log *zap.Logger) Persistence {
+	return Persistence{
+		payment: payment.InitStorage(db, log),
+	}
+}
