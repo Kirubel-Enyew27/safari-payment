961fdd20b450dd9151c5eb4b3bd94fb4b91af848
Kirubel Enyew <enyewkirubel76@gmail.com>
2025-08-08 01:11:41 +0300

    add SQL queries to generate type-safe Go code using sqlc

---
+++ internal/query/queries/payments.sql
@@ -0,0 +1,22 @@
+-- name: SavePayment :one
+INSERT INTO payments (
+    checkout_request_id,
+    merchant_request_id,
+    phone_number,
+    amount,
+    mpesa_receipt,
+    transaction_date,
+    result_code,
+    result_desc
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8
+)
+RETURNING *;
+
+-- name: GetPaymentByCheckoutRequestID :one
+SELECT * FROM payments
+WHERE checkout_request_id = $1;
+
+-- name: ListPayments :many
+SELECT * FROM payments
+ORDER BY created_at DESC;
